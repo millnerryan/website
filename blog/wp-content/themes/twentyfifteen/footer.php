8aea0f675e3e6633e67f25e6a5efbd769900f4ad
Ryan Millner <millner.ryan@gmail.com>
2015-03-15 17:48:32 -0700

    removing wp footer

--- blog/wp-content/themes/twentyfifteen/footer.php
+++ blog/wp-content/themes/twentyfifteen/footer.php
@@ -13,7 +13,7 @@
 	</div><!-- .site-content -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
+<!-- 		<div class="site-info">
 			<?php
 				/**
 				 * Fires before the Twenty Fifteen footer text for footer customization.
@@ -23,7 +23,7 @@
 				do_action( 'twentyfifteen_credits' );
 			?>
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentyfifteen' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentyfifteen' ), 'WordPress' ); ?></a>
-		</div><!-- .site-info -->
+		</div> --><!-- .site-info -->
 	</footer><!-- .site-footer -->
 
 </div><!-- .site -->
